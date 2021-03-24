2d7034e1e7a4aa0f8f71ded8bf89d575e82e16c1
canzoroglu <canzoroglu@gmail.com>
2021-03-24 23:12:59 +0300

    Add Person class

--- BMICalculator/Program.cs
+++ BMICalculator/Program.cs
@@ -6,30 +6,52 @@ namespace BMICalculator
     {
         static void Main(string[] args)
         {
-        	// To be able to use "." instead of "," as decimal separator
+            // To be able to use "." instead of "," as decimal separator
             System.Globalization.CultureInfo customCulture = (System.Globalization.CultureInfo)System.Threading.Thread.CurrentThread.CurrentCulture.Clone();
             customCulture.NumberFormat.NumberDecimalSeparator = ".";
             System.Threading.Thread.CurrentThread.CurrentCulture = customCulture;
 
-            for (int i = 0; i < 2; i++)
-            {
-                Console.WriteLine("Person " + (i+1));
-                Console.Write("Enter your name: ");
-                string name = Console.ReadLine();
-                Console.Write("Enter your surname: ");
-                string surname = Console.ReadLine();
-                Console.Write("Enter your age: ");
-                int age = int.Parse(Console.ReadLine());
-                Console.Write("Enter your weight in kg: ");
-                double weight = double.Parse(Console.ReadLine());
-                Console.Write("Enter your height in cm: ");
-                double heightC = double.Parse(Console.ReadLine());
-                double heightM = heightC / 100.0;
-                string msg = $"{name} {surname} is {age} years old, his weight is {weight} kg and his height is {heightC} cm.";
-                double bmi = weight / (heightM * heightM);
-                Console.WriteLine(msg);
-                Console.WriteLine($"{name}'s BMI is {bmi:F2}");
-            }
+            Person p1 = new Person();
+            p1.GetPersonInfo();
+            p1.PrintMsg();
+
+            Person p2 = new Person();
+            p2.GetPersonInfo();
+            p2.PrintMsg();
+        }
+
+    }
+
+    class Person
+    {
+        string name, surname;
+        int age;
+        double weight, height;
+        public void GetPersonInfo()
+        {
+            Console.Write("Enter your name: ");
+            this.name = Console.ReadLine();
+            Console.Write("Enter your surname: ");
+            this.surname = Console.ReadLine();
+            Console.Write("Enter your age: ");
+            this.age = int.Parse(Console.ReadLine());
+            Console.Write("Enter your weight in kg: ");
+            this.weight = double.Parse(Console.ReadLine());
+            Console.Write("Enter your height in cm: ");
+            this.height = double.Parse(Console.ReadLine());
+        }
+
+        public double CalculateBMI(double weight, double height)
+        {
+            return weight / (height * height);
+        }
+
+        public void PrintMsg()
+        {
+            string msg = $"{name} {surname} is {age} years old, his weight is {weight} kg and his height is {height} cm.";
+            double bmi = CalculateBMI(weight, height/100.0);
+            Console.WriteLine(msg);
+            Console.WriteLine($"{name}'s BMI is {bmi:F2}");
         }
     }
 }
