ce22915472b7cb992acd354f86e62a60033eff3a
bucephalus26 <ibrahimkhan13@hotmail.com>
2021-03-27 23:00:21 +0000

    This is my commit message

---
+++ Bootcamp lesson one homework - BMI/Bootcamp lesson one homework - BMI/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Bootcamp_lesson_one_homework___BMI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            infoAndBMI();
+            infoAndBMI();
+
+        }
+
+
+        static void infoAndBMI()
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter your age: ");
+            int age = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter your weight in KG: ");
+            double weight = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter your height in centimeters: ");
+            double height = double.Parse(Console.ReadLine());
+
+            printInfo(name, age, weight, height);
+            printBMI(weight, height);
+
+        }
+
+
+        static void printInfo(string name, int age, double weight, double height)
+        {
+            Console.WriteLine($"{name} is {age} years old, his weight is {weight} and his height is {height} cm.");
+            
+        }
+
+        static void printBMI(double weight, double height)
+        {
+            height /= 100;
+            double bmi = (weight / height) / height;
+
+            Console.WriteLine($"Persons BMI is: {bmi}");
+        }
+
+
+    }
+
+  
+}
