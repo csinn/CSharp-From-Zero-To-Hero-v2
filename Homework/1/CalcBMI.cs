996d0ef19ac3ba9b731079684c861433fd71986a
Laimonukas <38073858+Laimonukas@users.noreply.github.com>
2021-03-28 06:29:55 +0300

    Adding smaller functions

--- Homework/1/CalcBMI.cs
+++ Homework/1/CalcBMI.cs
@@ -12,7 +12,7 @@ namespace Homework1
             3. Calculate and print body-mass index (BMI)
             4. Do 1 and 2 for another person.
         */
-        
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello!\n");
@@ -23,35 +23,56 @@ namespace Homework1
         {
             Console.WriteLine("Please input name:");
             string personName = Console.ReadLine();
+
             Console.WriteLine("Surname:");
             string personSurname = Console.ReadLine();
-            Console.WriteLine("Age:");
-            if (!int.TryParse(Console.ReadLine(), out int personAge))
-            {
-                Console.WriteLine("Bad value input. Try again.\n");
-                RequestAndReadInput();
-                return;
-            }
-            Console.WriteLine("Weight(kg):");
-            if (!float.TryParse(Console.ReadLine(), out float personWeight))
-            {
-                Console.WriteLine("Bad value input. Try again.\n");
-                RequestAndReadInput();
-                return;
-            }
-            Console.WriteLine("Height(cm):");
-            if (!float.TryParse(Console.ReadLine(), out float personHeight))
-            {
-                Console.WriteLine("Bad value input. Try again.\n");
-                RequestAndReadInput();
-                return;
-            }
-            Console.WriteLine($"{personName} {personSurname} is {personAge} years old, his weight is {personWeight} kg and his height is {personHeight} cm.\n");
-            Console.WriteLine($"BMI is {personWeight / Math.Pow(personHeight / 100, 2):F2}");
+
+            float personWeight = GetPersonWeight();
+            float personHeight = GetPersonHeight();
+
+            Console.WriteLine($"{personName} {personSurname} is {GetPersonAge()} years old, his weight is {personWeight} kg and his height is {personHeight} cm.\n");
+            Console.WriteLine($"{personName}'s BMI is {personWeight / Math.Pow(personHeight / 100, 2):F2}");
+
             Console.WriteLine("Do you want to do another person? y/n");
             if (Console.ReadLine().ToLower() == "y")
             {
                 RequestAndReadInput();
+            }
+        }
+
+        private static int GetPersonAge() {
+            Console.WriteLine("Age:");
+            if (!int.TryParse(Console.ReadLine(), out int personAge))
+            {
+                Console.WriteLine("Bad value input. Try again.\n");
+                return GetPersonAge();
+            }
+            else {
+                return personAge;
+            }
+        }
+
+        private static float GetPersonWeight() {
+            Console.WriteLine("Weight(kg):");
+            if (!float.TryParse(Console.ReadLine(), out float personWeight))
+            {
+                Console.WriteLine("Bad value input. Try again.\n");
+                return GetPersonWeight();
+            }
+            else {
+                return personWeight;
+            }
+        }
+
+        private static float GetPersonHeight() {
+            Console.WriteLine("Height(cm):");
+            if (!float.TryParse(Console.ReadLine(), out float personHeight))
+            {
+                Console.WriteLine("Bad value input. Try again.\n");
+                return GetPersonHeight();
+            }
+            else {
+                return personHeight;
             }
         }
     }
