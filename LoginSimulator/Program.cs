fd283846e0eba3a8515c016cef4a5c32d88d48a6
Gregory Butler <gregorys.butlers@gmail.com>
2021-03-29 02:39:12 +0300

    add a new project for the login part.

---
+++ LoginSimulator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LoginSimulator
+{
+  internal static class Program
+  {
+    private static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+    }
+
+    private static string PrompString()
+    {
+      return default;
+    }
+  }
+}
