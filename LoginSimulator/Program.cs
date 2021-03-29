f81825bcaf23f43873d8c91b0f95bb87c392d0e9
Gregory Butler <gregorys.butlers@gmail.com>
2021-03-29 03:41:25 +0300

    implement 'PromtString' method that reads user input without key echo; implement 'TestCredentials' method test user input against stored credential.

--- LoginSimulator/Program.cs
+++ LoginSimulator/Program.cs
@@ -6,12 +6,50 @@ namespace LoginSimulator
   {
     private static void Main(string[] args)
     {
-      Console.WriteLine("Hello World!");
+      TestCredentials();
     }
 
-    private static string PrompString()
+    private static void TestCredentials()
     {
-      return default;
+      const string storedUserName = "tEsT";
+      const string storedUserPassword = "1234";
+      
+      const string successfulLoginMessage = "Logged in!";
+      
+      var userName = PromptString("Username: ");
+      var userPassword = PromptString("Password: ");
+
+      var isValidCredential = string.Equals(userName, storedUserName, StringComparison.OrdinalIgnoreCase) &&
+                              string.Equals(userPassword, storedUserPassword);
+      if (isValidCredential)
+      {
+        Console.WriteLine(successfulLoginMessage);
+      }
+    }
+    private static string PromptString(string message, bool isKeyVisible = false)
+    {
+      if (string.IsNullOrWhiteSpace(message))
+      {
+        throw new ArgumentException("Value cannot be null or empty.", nameof(message));
+      }
+      
+      Console.Write(message);
+      
+      var output = "";
+      while (true)
+      {
+        var pressedKey = Console.ReadKey(!isKeyVisible);
+        if (pressedKey.Key == ConsoleKey.Enter)
+        {
+          break;
+        }
+
+        output += pressedKey.KeyChar;
+      }
+
+      Console.WriteLine();
+
+      return output;
     }
   }
 }
