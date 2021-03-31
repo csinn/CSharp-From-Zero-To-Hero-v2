5f3675694a45e38304f9d0c75066626a8c9e52dc
Gregory Butler <gregorys.butlers@gmail.com>
2021-03-31 23:51:13 +0300

    implement tests for 'InsetAt' method.

--- ArrayOperationTests/InsertAtTests.cs
+++ ArrayOperationTests/InsertAtTests.cs
@@ -7,6 +7,43 @@ namespace ArrayOperationTests
 {
   public class InsertAtTests
   {
-    
+    [Fact]
+    public void InsertAt_Should_Throw_ArgumentException_When_Input_Array_Is_Empty()
+    {
+      var input = new int[] {};
+      var element = 1;
+      var index = 0;
+      
+      Action test = () => Arrays.InsertAt(input,element, index);
+      
+      Assert.Throws<ArgumentException>(test);
+    }
+
+    [Fact]
+    public void InsertAt_Should_Throw_ArgumentNullException_When_Input_Is_Null()
+    {
+      int[] input = null;
+      var element = 1;
+      var index = 0;
+      
+      Action test = () => Arrays.InsertAt(input,element, index);
+      
+      Assert.Throws<ArgumentNullException>(test);
+    }
+
+    [Theory]
+    [InlineData(new[] {0,2,3}, 1, 1, new[] {0, 1, 2, 3})]
+    [InlineData(new[] {1,2,3}, 0, 0, new[] {0, 1, 2, 3})]
+    [InlineData(new[] {0,1,2}, 3, 3, new[] {0, 1, 2, 3})]
+    [InlineData(new[] {0},1,1, new[] {0,1})]
+    [InlineData(new[] {1},0,0, new[] {0,1})]
+    public void InsertAt_Should_Return_New_Array_With_Item_Inserted_At_Correct_Position(int[] input, int number, int index, int[] expected)
+    { 
+      int[] test; 
+       
+      test = Arrays.InsertAt(input, number, index);
+     
+      Assert.Equal(expected, test);
+    }
   }
 }
