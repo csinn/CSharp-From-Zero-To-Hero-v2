8a479ec475dcbeb70c5e5c85d9a128352ca628cc
Gregory Butler <gregorys.butlers@gmail.com>
2021-04-01 18:20:05 +0300

    at test to cover 'ArgumentOutOfRangeException'.

--- ArrayOperationTests/InsertAtTests.cs
+++ ArrayOperationTests/InsertAtTests.cs
@@ -30,6 +30,19 @@ namespace ArrayOperationTests
       
       Assert.Throws<ArgumentNullException>(actual);
     }
+    
+    [Theory]
+    [InlineData(new[] {0, 1, 2}, 4)]
+    [InlineData(new[] {0, 1, 2}, -1)]
+    public void InsertAt_Should_Throw_ArgumentOutOfRange_When_Index_Is_Outside_Of_Array_Bounds(int[] input, int index)
+    {
+      const int item = 9;
+      Action actual;
+        
+      actual = () => Arrays.InsertAt(input, item, index);
+
+      Assert.Throws<ArgumentOutOfRangeException>(actual);
+    }
 
     [Theory]
     [InlineData(new[] {0,2,3}, 1, 1, new[] {0, 1, 2, 3})]
