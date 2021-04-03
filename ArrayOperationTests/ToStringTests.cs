b0520da0f3967596cb633390108e3e27f42d4ca6
Gregory Butler <gregorys.butlers@gmail.com>
2021-04-04 02:47:53 +0300

    implement tests for 'ToString' method.

---
+++ ArrayOperationTests/ToStringTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using ArrayOperations;
+using FluentAssertions;
+using Xunit;
+
+namespace ArrayOperationTests
+{
+  public class ToStringTests
+  {
+    [Fact]
+    public void ToString_Should_Throw_ArgumentNullException_When_Input_Array_Is_Null()
+    {
+      int[] input = null;
+
+      Action actual = () => Arrays.ToString(input);
+
+      actual.Should().ThrowExactly<ArgumentNullException>();
+    }
+
+    [Theory]
+    [InlineData(new[] {1}, "[ 1 ]")]
+    [InlineData(new[] {0, 1}, "[ 0, 1 ]")]
+    [InlineData(new int[] { }, "[ ]")]
+    public void ToString_Should_Return_Expected_Data(int[] input, string expected)
+    {
+      var actual = Arrays.ToString(input);
+
+      actual.Should().Be(expected);
+    }
+  }
+}
