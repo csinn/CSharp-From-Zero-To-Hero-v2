b4c99bc5c269ff6a056ef5b166bb2a2ee44257dd
Gregory Butler <gregorys.butlers@gmail.com>
2021-04-05 15:57:51 +0300

    implement 'ReadAllText' and 'ReadAllLines' methods.

--- Credentials/Files.cs
+++ Credentials/Files.cs
@@ -1,6 +1,22 @@
-﻿namespace Credentials
+﻿using System;
+using System.IO;
+
+namespace Credentials
 {
-  public class Files
+  public static class Files
   {
+    public static string ReadAllText(string file)
+    {
+      using (var reader = new StreamReader(file))
+      {
+        return reader.ReadToEnd().Trim();
+      }
+    }
+
+    public static string[] ReadAllLines(string file)
+    {
+      var content = ReadAllText(file);
+      return content.Split($"{Environment.NewLine}");
+    }
   }
 }
