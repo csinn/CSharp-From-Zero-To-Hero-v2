c7d057cff9457fb1da9310c7584852d857e5399b
Gregory Butler <gregorys.butlers@gmail.com>
2021-04-05 15:59:06 +0300

    implement tests for 'ReadAllText' and 'ReadAllLines' methods.

--- CredentialsTests/FilesTests.cs
+++ CredentialsTests/FilesTests.cs
@@ -1,12 +1,31 @@
+using Credentials;
+using FluentAssertions;
 using Xunit;
 
 namespace CredentialsTests
 {
   public class FilesTests
   {
-    [Fact]
-    public void Test1()
+    [Theory]
+    [InlineData(@"Input/read1.txt", "Test\r\nLine2")]
+    [InlineData(@"Input/empty.txt", "")]
+    [InlineData(@"Input/emptylines.txt", "")]
+    public void ReadAllText_Should_Return_Expected_Output(string file, string expected)
     {
+      var actual = Files.ReadAllText(file);
+
+      actual.Should().Be(expected);
+    }
+
+    [Theory]
+    [InlineData(@"Input/read1.txt", new[] {"Test", "Line2"})]
+    [InlineData(@"Input/empty.txt", new[] {""})]
+    [InlineData(@"Input/emptylines.txt", new[] {""})]
+    public void ReadAllLines_Should_Return_Expected_Output(string file, string[] expected)
+    {
+      var actual = Files.ReadAllLines(file);
+
+      actual.Should().Equal(expected);
     }
   }
 }
