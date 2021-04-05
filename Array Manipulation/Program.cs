36ca66025857deab2f22154ecd47b872ab268a71
AayushM-Upes <54975859+AayushM-upes@users.noreply.github.com>
2021-04-05 23:57:28 +0530

    not working method RemoveFromStart
    Incomplete homework lesson 2

--- Array Manipulation/Program.cs
+++ Array Manipulation/Program.cs
@@ -6,7 +6,95 @@ namespace Array_Manipulation
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] arr = {1, 2, 4, 5};
+
+            PrintArray(arr);
+            Console.WriteLine();
+
+            InsertAtEnd(arr, 32);
+            Console.WriteLine();
+
+            InsertAtStart(arr, 32);
+            Console.WriteLine();
+
+            RemoveFromEnd(arr);
+            Console.WriteLine();
+
+            RemoveFromStart(arr);
+        }
+
+        static void PrintArray(int[] arr)
+        {
+            Console.Write("Array: ");
+            foreach (var variable in arr)
+            {
+                Console.Write($"{variable} ");
+            }
+        }
+
+        static void ArraySort(int[] arr)
+        {
+
+        }
+
+        static void InsertAtEnd(int[] arr, int value)
+        {
+            int newLength = arr.Length + 1;
+            int[] arrFinal = new int[newLength];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arrFinal[i] = arr[i];
+            }
+
+            arrFinal[newLength - 1] = value;
+
+            Console.Write("After Insertion At the End: ");
+            PrintArray(arrFinal);
+        }
+
+        static void InsertAtStart(int[] arr, int value)
+        {
+            int newLength = arr.Length + 1;
+            int[] arrFinal = new int[newLength];
+
+            for (int i = arr.Length; i > 0; i--)
+            {
+                arrFinal[i] = arr[i-1];
+            }
+
+            arrFinal[0] = value;
+
+            Console.Write("After Insertion At the Start: ");
+            PrintArray(arrFinal);
+        }
+
+        static void RemoveFromEnd(int[] arr)
+        {
+            int newLength = arr.Length - 1;
+            int[] arrFinal = new int[newLength];
+
+            for (int i = 0; i < arrFinal.Length; i++)
+            {
+                arrFinal[i] = arr[i];
+            }
+
+            Console.Write("After Removing From the End: ");
+            PrintArray(arrFinal);
+        }
+
+        static void RemoveFromStart(int[] arr)
+        {
+            int newLength = arr.Length - 1;
+            int[] arrFinal = new int[newLength];
+
+            for (int i = arrFinal.Length - 1; i <= 0; i--)
+            {
+                arrFinal[i] = arr[i + 1];
+            }
+
+            Console.Write("After Removing From the Start: ");
+            PrintArray(arrFinal);
         }
     }
 }
