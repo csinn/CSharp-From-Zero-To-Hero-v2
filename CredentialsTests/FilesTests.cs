697817b402321d05a0f5a143b1a253434afbbddf
Gregory Butler <gregorys.butlers@gmail.com>
2021-04-06 00:08:00 +0300

    move mocking files to another dir.

--- CredentialsTests/FilesTests.cs
+++ CredentialsTests/FilesTests.cs
@@ -9,10 +9,10 @@ namespace CredentialsTests
   public class FilesTests
   {
     [Theory]
-    [InlineData(@"Input/read1.txt", "Test\r\nLine2")]
-    [InlineData(@"Input/read2.txt", "Test\nLine2")]
-    [InlineData(@"Input/empty.txt", "")]
-    [InlineData(@"Input/emptylines.txt", "")]
+    [InlineData(@"Files/Input/read1.txt", "Test\r\nLine2")]
+    [InlineData(@"Files/Input/read2.txt", "Test\nLine2")]
+    [InlineData(@"Files/Input/empty.txt", "")]
+    [InlineData(@"Files/Input/emptylines.txt", "")]
     public void ReadAllText_Should_Return_Expected_Output(string file, string expected)
     {
       var actual = Files.ReadAllText(file);
@@ -21,9 +21,9 @@ namespace CredentialsTests
     }
 
     [Theory]
-    [InlineData(@"Input/read1.txt", new[] {"Test", "Line2"})]
-    [InlineData(@"Input/empty.txt", new[] {""})]
-    [InlineData(@"Input/emptylines.txt", new[] {""})]
+    [InlineData(@"Files/Input/read1.txt", new[] {"Test", "Line2"})]
+    [InlineData(@"Files/Input/empty.txt", new[] {""})]
+    [InlineData(@"Files/Input/emptylines.txt", new[] {""})]
     public void ReadAllLines_Should_Return_Expected_Output(string file, string[] expected)
     {
       var actual = Files.ReadAllLines(file);
@@ -32,10 +32,10 @@ namespace CredentialsTests
     }
 
     [Theory]
-    [InlineData(@"Output/expected1.txt", "Test.w\r\nLine2.w")]
-    [InlineData(@"Output/expected_empty.txt", "")]
-    [InlineData(@"Output/expected_empty2.txt", "\r\n\r\n\r\n")]
-    [InlineData(@"Output/expected_empty_3.txt", "\n\n\n")]
+    [InlineData(@"Files/Output/expected1.txt", "Test.w\r\nLine2.w")]
+    [InlineData(@"Files/Output/expected_empty.txt", "")]
+    [InlineData(@"Files/Output/expected_empty2.txt", "\r\n\r\n\r\n")]
+    [InlineData(@"Files/Output/expected_empty_3.txt", "\n\n\n")]
     public void WriteAllText_Should_Create_Expected_Output(string file, string expected)
     {
       File.Delete(file);
@@ -47,9 +47,9 @@ namespace CredentialsTests
     }
 
     [Theory]
-    [InlineData(@"Output/expected_lines1.txt", new[] {"Test.l", "Line2.l"})]
-    [InlineData(@"Output/expected_lines_empty.txt", new string[0])]
-    [InlineData(@"Output/expected_lines_empty2.txt", new[] {"", "", ""})]
+    [InlineData(@"Files/Output/expected_lines1.txt", new[] {"Test.l", "Line2.l"})]
+    [InlineData(@"Files/Output/expected_lines_empty.txt", new string[0])]
+    [InlineData(@"Files/Output/expected_lines_empty2.txt", new[] {"", "", ""})]
     public void WriteAllLines_Should_Create_Expected_Output(string file, string[] expected)
     {
       File.Delete(file);
@@ -61,8 +61,8 @@ namespace CredentialsTests
     }
 
     [Theory]
-    [InlineData(@"Output/expected_single_line.txt", "AllTheSingleLines")]
-    [InlineData(@"Output/expected_multi_line.txt", "Line1", true)]
+    [InlineData(@"Files/Output/expected_single_line.txt", "AllTheSingleLines")]
+    [InlineData(@"Files/Output/expected_multi_line.txt", "Line1", true)]
     public void WriteLine_Should_Create_Expected_Output(string file, string expected, bool append = false)
     {
       File.Delete(file);
