a909e0387f012b7b9cd31e992826772015297a51
Gregory Butler <gregorys.butlers@gmail.com>
2021-04-06 04:43:22 +0300

    disable 'RCS1194'.

--- CredentialsManager/DuplicateUserCredentialsException.cs
+++ CredentialsManager/DuplicateUserCredentialsException.cs
@@ -2,7 +2,10 @@
 
 namespace CredentialsManager
 {
+#pragma warning disable RCS1194 // Implement exception constructors.
+
   public class DuplicateUserCredentialsException : Exception
+#pragma warning restore RCS1194 // Implement exception constructors.
   {
     public DuplicateUserCredentialsException()
     {
