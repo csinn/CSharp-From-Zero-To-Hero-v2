284726af415976e95c4236914cf770918269110d
Gregory Butler <gregorys.butlers@gmail.com>
2021-04-06 04:40:22 +0300

    implement exception.

---
+++ CredentialsManager/UserNameIsTakenException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CredentialsManager
+{
+#pragma warning disable RCS1194 // Implement exception constructors.
+  public class UserNameIsTakenException : Exception
+#pragma warning restore RCS1194 // Implement exception constructors.
+  {
+    public UserNameIsTakenException() : base()
+    {
+    }
+
+    public UserNameIsTakenException(string? message) : base(message)
+    {
+    }
+  }
+}
