a6a30e6c84feab76edafc6ebf494ccab7205e58f
DRNK_Wolf <54975859+AayushM-upes@users.noreply.github.com>
2021-04-25 16:37:44 +0530

    Completed HomeWork

--- LogIn_HomeWork_Week2/Program.cs
+++ LogIn_HomeWork_Week2/Program.cs
@@ -4,9 +4,47 @@ namespace LogIn_HomeWork_Week2
 {
     class Program
     {
+        public static string[] UserNames = new string[] {"CrowBar", "MasterOgre", "redClown", "bLLoom"};
+        public static string[] PassWords = new string[] {"1234", "meNace", "mYworld", "forwarDash"};
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] userInfo = GetUserInfo();
+            VerifyUserInfo(userInfo);
+            
+        }
+
+        static string[] GetUserInfo()
+        {
+            var info = new string[2];
+
+            Console.Write("Enter Your\nUser Name: ");
+            info[0] = Console.ReadLine();
+
+            Console.Write("Enter Your\nPassword: ");   
+            info[1] = Console.ReadLine();
+
+            return info;
+
+        }
+
+        static void VerifyUserInfo(string[] info)
+        {
+            for (int i = 0; i < UserNames.Length; i++)
+            {
+                if (UserNames[i].Equals(info[0], StringComparison.OrdinalIgnoreCase))
+                {
+                    if (PassWords[i].Equals(info[1]))
+                    {
+                        Console.WriteLine("Logged in!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Incorrect Password or User name!");
+                        break;
+                    }
+                }
+            }
         }
     }
+
 }
