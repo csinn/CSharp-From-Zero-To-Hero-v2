fb7075b473998b5af4682f2e3e650e6eb469edc9
DRNK_Wolf <54975859+AayushM-upes@users.noreply.github.com>
2021-04-25 15:21:12 +0530

    Added a seperate project for Login part of the homework

---
+++ LogIn_HomeWork_Week2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LogIn_HomeWork_Week2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
