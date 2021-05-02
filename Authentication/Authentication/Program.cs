0e46866381d43d86548f739c2bd042aebb96bf5d
DRNK_Wolf <54975859+AayushM-upes@users.noreply.github.com>
2021-05-02 15:00:23 +0530

    Added Users.txt and created method Login

--- Authentication/Authentication/Program.cs
+++ Authentication/Authentication/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection;
 
 namespace Authentication
 {
@@ -6,7 +7,11 @@ namespace Authentication
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+        }
+
+        public void Login()
+        {
+
         }
     }
 }
