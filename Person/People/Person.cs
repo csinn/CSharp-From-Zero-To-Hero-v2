f0abbdb9e408e431b8f6b1d1176f3913ecbef97c
aus10Code <aus10.code@gmail.com>
2021-05-07 12:58:33 -0500

    Homework 5

---
+++ Person/People/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Person
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public String Gender { get; set; }
+        public int Age { get; set; }
+        public int SpouseId { get; set; }
+        public List<Pet> PetList { get; set; } = new List<Pet>();
+    }
+}
---
+++ Person/People/Pet.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Person
+{
+    public class Pet
+    {
+        public String Type { get; set; }
+        public String Name { get; set; }
+    }
+}
