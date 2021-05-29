b17b81ff86c1c29c33d8b3271793b7417058cb59
Tailor <kralle37@gmail.com>
2021-05-29 20:51:02 +0200

    Start of homework 8.

---
+++ ElectricBillApi/Controllers/ElectricProviderController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ElectricBillApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ElectricProviderController : ControllerBase
+    {
+        
+    }
+}
