cb6538897edcf53e3fb9ae5aae40873d20e48cc9
cjahermanns <c.j.a.hermanns@gmail.com>
2021-05-30 23:53:35 +0200

    Added test base

---
+++ ElectricityBillApi.Tests/ElectricProviderTests.cs
@@ -0,0 +1,22 @@
+﻿using ElectricityBillApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectricityBillApi.Tests
+{
+    public class ElectricProviderTests
+    {
+        private ElectricProvider _sut;
+
+        public ElectricProviderTests()
+        {
+            _sut = new ElectricProvider()
+            {
+                Name = "Test Provider"
+            };
+        }
+    }
+}
