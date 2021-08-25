e16e5117ecb64d57aedf8493661ca278ddb7c443
Roelof Wobben <rwobben@hotmail.com>
2021-08-25 20:47:00 +0200

    Deleted Array_Functions Class make seperate method so no code is duplocted make all variables snakeCase

--- Chapter1/BMI_Calculator.cs
+++ Chapter1/BMI_Calculator.cs
@@ -9,8 +9,13 @@ namespace BootCamp.Chapter1
     {
         public static void BMI_Calculator()
         {
-            // Person 1 
 
+            CalculateBMI();
+            CalculateBMI(); 
+        }
+
+        private static void CalculateBMI()
+        {
             Console.Write("Can I have your name: ");
             var name = Console.ReadLine();
             Console.Write("Can I have your surname: ");
@@ -24,36 +29,15 @@ namespace BootCamp.Chapter1
 
             Console.WriteLine($"{name} {surname} is {age} years old, his weight is {weight} kg and his height is {height} cm.");
 
-            var BMI = calculate(height, weight);
+            var BMI = Calculate(height, weight);
             Console.WriteLine($"This person bmi is {BMI}");
-
-            // Person 2 
-
-            Console.Write("Can I have your name: ");
-            var name2 = Console.ReadLine();
-            Console.Write("Can I have your surname: ");
-            var surname2 = Console.ReadLine();
-            Console.Write("Can I have your age:");
-            var age2 = Console.ReadLine();
-            Console.Write("Can I have your weight in kg: ");
-            var weight2 = Console.ReadLine();
-            Console.Write("Can I have your height in cm: ");
-            var height2 = Console.ReadLine();
-
-            Console.WriteLine($"{name2} {surname2} is {age2} years old, his weight is {weight2} kg and his height is {height2} cm.");
-
-            BMI = calculate(height, weight);
-            Console.WriteLine($"This person bmi is {BMI}");
-
-
-
         }
 
-        private static double calculate(string height, string weight)
+        private static double Calculate(string height, string weight)
         {
-            var parsed_height = double.Parse(height) / 100;
-            var parsed_weight = double.Parse(weight);
-            return parsed_weight / (parsed_height * parsed_height);
+            var parsedHeight = double.Parse(height) / 100;
+            var parsedWeight = double.Parse(weight);
+            return parsedWeight / (parsedHeight * parsedHeight);
         }
     }
 }
