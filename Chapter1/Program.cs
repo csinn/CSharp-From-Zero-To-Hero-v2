3355f502aa34705ec1b4bdb7b3a72119612cf274
Roelof Wobben <rwobben@hotmail.com>
2021-08-26 12:15:21 +0200

    a few changes requested

---
+++ Chapter1/Program.cs
@@ -0,0 +1,23 @@
+﻿using BootCamp.Chapter1;
+using System;
+
+namespace Chapter1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Call BMI_Calculator
+
+            BMICalculator.BMI_Calculator(); 
+                       
+
+            Console.Read();
+
+
+
+        }
+
+
+    }
+}
