9ea8d303afff17fe666e7248da6ba3cbf6749748
vytcka <vytas.sa1@gmail.com>
2022-07-16 21:56:10 +0300

    First homework. Asking 2 people for their information and finding out their BMI score.

--- RecipeApp/Program.cs
+++ RecipeApp/Program.cs
@@ -1,110 +1,77 @@
-﻿// Import classes from system namespace
-using System;
+﻿using System;
 
-// Holder of classes
-namespace RecipeApp
+namespace FirstLessonHomework
 {
-    // Here is a list of standard cooking units:
-    //    Cup	0.24 l
-    //    Gallon  3.79 l
-    //    Fluid Ounce	29.57 ml
-    //    Pint    0.47 l
-    //    Quart   0.95 l
-    //    Tablespoon  14.79 ml
-    //    Teaspoon    4.93 ml
-
-    // Holder of functions and data
-    class Program
+    internal class Program
     {
-        // The less a function implements itself- the higher level it is
-        // Main function simply the application- so it is the highest level function
-        // It just consumes other functions.
-
-        // Named block of code
-        // cw + tab autocompletes to Console.WriteLine();
-        // strongly typed means that I don't need to guess what I can do with a class or a variable
-        // So, when I get errors, they will be before my application runs- in compile time.
-        // Weekly typed means that there is no check on what a function or variable cna do.
-        // We can do that with dynamic keyword: dynamic a; a.IsPerfectly.Fine().CsharpCode().
         static void Main(string[] args)
         {
-            double totalMl = PrintTeaspoonsToMl() + PrintTablespoonsToMl();
-            PrintHowMany100MlBottles(totalMl);
+            ReadAndPrintTwoPeoplesInformation();
         }
 
-        static double PrintTeaspoonsToMl()
+        static void ReadAndPrintTwoPeoplesInformation()
         {
-            var teaspoons = GetAmountFromConsole("teaspoons");
-            var mlOfTeaspoons = TeaspoonsToMl(teaspoons);
-            Print(teaspoons, "teaspoons", mlOfTeaspoons);
-
-            return mlOfTeaspoons;
+            ReadAndPrintPersonsInformation();
+            Console.WriteLine("");
+            ReadAndPrintPersonsInformation();
         }
 
-        static double PrintTablespoonsToMl()
+        static void ReadAndPrintPersonsInformation()
         {
-            double tablespoons = GetAmountFromConsole("tablespoons");
-            double mlOfTablespoons = TableSpoonsToMl(tablespoons);
-            Print(tablespoons, "tablespoons", mlOfTablespoons);
+            string name = PromptStringInput("What's your name?");
+            string surname = PromptStringInput("What's your surname?");
+            int age = PromptIntegerInput("What's your age?");
+            double weightInKg = PromptDoubleInput("What's your weight in kilograms?");
+            double heightInCm = PromptDoubleInput("What's your height in centimeters?");
 
-            return mlOfTablespoons;
+            PrintPersonsDescription(name, surname, age, weightInKg, heightInCm);
+
+            double bmi = CalculateBmiInMetricSystem(weightInKg, heightInCm);
+            PrintPersonsBMI(bmi);
         }
 
-        static void PrintHowMany100MlBottles(double ml)
+        static string PromptStringInput(string question)
         {
-            // 1 / 0 = error
-            // 1 / 2 = 0
-            // int / int = int
-            // 1.0 / 2 = 0.5
-            // double / int = double
-            // 1.0 + 1 = 2.0
-            // double + int = double
-            // We have bottles of 100 ml.
-            // How many bottles can we fill after the conversion?
-            // int is a whole number
-            int bottlesCount = (int)(ml / 100) + 1;
-            Console.WriteLine($"{ml:F2} can fill {bottlesCount} bottles of 100ml");
+            string textToWrite = $"{question} ";
+            Console.Write(textToWrite);
+
+            string textInput = Console.ReadLine();
+            return textInput;
         }
 
-        // GetTeaspoonsFromConsole is a lower level function.
-        // Because it implement HOW we can read a number from a console.
-        static double GetAmountFromConsole(string unit)
+        static int PromptIntegerInput(string question)
         {
-            Console.WriteLine($"Please enter {unit} amount: ");
-            // variable
-            // holds data
-            // type, name, value
-            var amountText = Console.ReadLine();
-            // Floating point number with double precision.
-            double amount = double.Parse(amountText);
-
-            return amount;
+            string textInput = PromptStringInput(question);
+            int numberInput = int.Parse(textInput);
+            return numberInput;
         }
 
-        static double TeaspoonsToMl(double teaspoons)
+        static double PromptDoubleInput(string question)
         {
-            // 4.93m is a decimal - use it for money- for absolute accuracy- use decimal.
-            // 4.93 is double
-            // 4.93f is a float- use it when you don't plan to use math.
-            double ml = teaspoons * 4.93;
-
-            return ml;
+            string textInput = PromptStringInput(question);
+            double doubleInput = double.Parse(textInput);
+            return doubleInput;
         }
 
-        static double TableSpoonsToMl(double teaspoons)
+        static void PrintPersonsDescription(string name, string surname, int age, double weightInKg, double heightInCm)
         {
-            double tablespoons = teaspoons * 14.79;
-
-            return tablespoons;
+            string informationAboutPerson = $"{name} {surname} is {age} years old, his weight is {weightInKg:F2} kg and his height is {heightInCm:F2} cm.";
+            Console.WriteLine(informationAboutPerson);
         }
 
-        static void Print(double cookingUnitAmount, string cookingUnit, double ml)
+        static double CalculateBmiInMetricSystem(double weightInKg, double heightInCm)
         {
-            // string convertedDescription = teaspoonsText + " teaspoons = " + ml + " ml";
-            // $- string interpolation
-            // F2- 2 digits after decimal point.
-            string convertedDescription = $"{cookingUnitAmount} {cookingUnit} = {ml:F2} ml";
-            Console.WriteLine(convertedDescription);
+            double heightInMeters = heightInCm / 100;
+            double heightInSquaredMeters = heightInMeters * heightInMeters;
+            
+            double bmi = weightInKg / heightInSquaredMeters;
+            return bmi;
+        }
+
+        static void PrintPersonsBMI(double bmi)
+        {
+            string textToPrint = $"His BMI score is: {bmi:F1} kg/m^2";
+            Console.WriteLine(textToPrint);
         }
     }
 }
